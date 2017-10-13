73ca119126e02268ada26151b94cc895d4720a78
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-10-14 00:41:54 +0500

    working...

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,13 +4,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 
+	public Welcome(){
+		$this->load->library('session');
+		$this->seesion->set_userdata('key','value');
+	}
+
 	public function index()
 	{
 		$this->load->view('welcome_message');
-	}
+			}
 
 	public function testing(){
 		echo "Testing";
+		$value = $this->session->userdata('key');
+		echo $value;
 	}
 }
 ?>
